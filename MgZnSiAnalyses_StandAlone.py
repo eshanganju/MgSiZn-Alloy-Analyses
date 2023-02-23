ab0c15d82387cfabf89fa72fab8c9af5849ed75f
eshanganju <ganju.eshan@gmail.com>
2023-02-22 20:25:22 -0500

    adding code for analyses - standalone script

--- MgZnSiAnalyses_StandAlone.py
+++ MgZnSiAnalyses_StandAlone.py
@@ -1,8 +1,8 @@
-"""
+"""Code for the analyses of MgZnSi particles
 """
 
-from pac import Segment
-from scipy.ndimage.morphology import distance_transform_edt as edt
+
+from scipy.ndimage import distance_transform_edt as edt
 import numpy as np
 import tifffile as tf
 from skimage.morphology import skeletonize
@@ -12,23 +12,25 @@ from scipy import ndimage
 from skan import Skeleton, summarize
 import pandas as pd
 from numba import jit
+import matplotlib.pyplot as plt
+from skimage.measure import marching_cubes, mesh_surface_area
+import trimesh
 
 VERBOSE = True
 
-clm = '/home/eg/Desktop/EG-WateshedAnalysesAvizo/2023-01-27-B-2-7_LE_recon-borderKill.tif'	# input('Enter label map location: ')
-ofl = '/home/eg/Desktop/EG-WateshedAnalysesAvizo/D-2-7/' 									# input('Enter output folder location: ')
-sampleName = 'D-2-7'																		# input('Enter sample name: ')
+clm = '/home/eg/Desktop/MgZnSi-Analyses/Clean-B-2-7.tif'	# input('Enter label map location: ')
+ofl = '/home/eg/Desktop/MgZnSi-Analyses/outPut/B-2-7/' 		# input('Enter output folder location: ')
+sampleName = 'B-2-7'										# input('Enter sample name: ')
 
 genSTL=False
-genEDM=False
-genEDMSkeleton=False
 
+genEDM=True
 genSkeleton=True
 genPtclVol=True
 genSaPtcl=True
 genHullData = True
 
-
+# ok
 def convexHullDataOfParticle(particleMap, dilateParticle=False):
 	"""Get convex hull of the particle
 	"""
@@ -46,56 +48,8 @@ def convexHullDataOfParticle(particleMap, dilateParticle=False):
 
 	return area, volume
 
-@jit(nopython=True)
-def cropAndPadParticle(labelledMap,label,pad, saveData=True,fileName='',outputDir=''):
-	"""
-	"""
-	loc = np.where(labelledMap == label)
-	dimZ = int(loc[0].max() - loc[0].min())
-	dimY = int(loc[1].max() - loc[1].min())
-	dimX = int(loc[2].max() - loc[2].min())
-	
-	croppedParticle = labelledMap[ loc[0].min() : loc[0].max()+1,
-									loc[1].min() : loc[1].max()+1,
-									loc[2].min() : loc[2].max()+1 ]
-
-	sizeZ = dimZ + 2 * pad
-	sizeY = dimY + 2 * pad
-	sizeX = dimX + 2 * pad
-
-	paddedParticle = np.zeros( ( sizeZ,sizeY,sizeX ) )
-	
-	paddedParticle[ pad : dimZ+pad+1, pad : dimY+pad+1, pad : dimX+pad+1] = croppedParticle
-
-	cleanedPaddedParticle = removeOtherLabels(paddedParticle, label)
-
-	# convert label number to ones
-	normalizedCleanedPaddedParticle = cleanedPaddedParticle//label
-
-	return normalizedCleanedPaddedParticle
-
-@jit(nopython=True)
-def removeOtherLabels(paddedParticle, label):
-	"""
-	"""
-	return np.where(np.logical_or(paddedParticle > label, paddedParticle < label), np.zeros_like(paddedParticle), paddedParticle)
-
-def _generateInPlaceStlFile(labMap, stepSize = 1, saveImg=True, sampleName='', outputDir=''):
-	"""Generate in place stl
-	"""
-	print('\tGenerating stl' )
-	
-	verts, faces, normals, values = marching_cubes( labMap, step_size=stepSize)
-
-	mesh = trimesh.Trimesh(verts,faces)
-	
-	smoothMesh = trimesh.smoothing.filter_humphrey(mesh)
-			
-	sampleName = outputDir + sampleName + '.stl'
-	smoothMesh.export(sampleName) 
-
-def obtainEuclidDistanceMap( binaryMapForEDM, scaleUp = int(1), 
-								saveImg=False, sampleName='', outputDir='' ):
+#ok
+def obtainEuclidDistanceMap( binaryMapForEDM, scaleUp = int(1), saveImg=False, sampleName='', outputDir='' ):
 	"""Computes the euclidian distance tranform (EDT) for a binary map
 
 	An EDT or an euclidian distance map (EDM) is an ndarray of the same size as
@@ -131,35 +85,89 @@ def obtainEuclidDistanceMap( binaryMapForEDM, scaleUp = int(1),
 
 	"""
 	if VERBOSE:
-		print('\nFinding Euclidian distance map (EDM)')
-		print('------------------------------------*')
+		print('\tFinding Euclidian distance map (EDM)')
+		print('\t------------------------------------*')
 
 	edMap = edt( binaryMapForEDM )
 
 	if scaleUp!=0 :
 		edMap =  edMap * scaleUp
 
-	if VERBOSE: print( "EDM Created" )
+	if VERBOSE: print( "\t\tEDM Created" )
 
 	if saveImg == True:
-		if VERBOSE: print('\nSaving EDM map...')
+		if VERBOSE: print('\t\tSaving EDM map...')
 		tf.imsave(outputDir + sampleName + '-edm.tif',edMap)
 
 	return edMap
 
+#ok
+def generateInPlaceStlFile(labMap, stepSize = 1, saveImg=True, sampleName='', outputDir=''):
+	"""Generate in place stl
+	"""
+	print('\tGenerating stl' )
+	
+	verts, faces, normals, values = marching_cubes( labMap, step_size=stepSize)
 
-def analyzeParticles(labMapLoc, sampleName='', saveData=True, outputDir='',):
-	"""Code for the analyses of label map
+	mesh = trimesh.Trimesh(verts,faces)
+	
+	smoothMesh = trimesh.smoothing.filter_humphrey(mesh)
+			
+	sampleName = outputDir + sampleName + '.stl'
+	smoothMesh.export(sampleName) 
+
+#ok
+@jit(nopython=True)
+def removeOtherLabels(paddedParticle, label):
+	"""
+	"""
+	return np.where(np.logical_or(paddedParticle > label, paddedParticle < label), np.zeros_like(paddedParticle), paddedParticle)
+
+#ok
+@jit(nopython=True)
+def cropAndPadParticle(labelledMap,label,pad, saveData=True,fileName='',outputDir=''):
+	"""
+	"""
+	loc = np.where(labelledMap == label)
+	dimZ = int(loc[0].max() - loc[0].min())
+	dimY = int(loc[1].max() - loc[1].min())
+	dimX = int(loc[2].max() - loc[2].min())
+	
+	croppedParticle = labelledMap[ loc[0].min() : loc[0].max()+1,
+									loc[1].min() : loc[1].max()+1,
+									loc[2].min() : loc[2].max()+1 ]
+
+	sizeZ = dimZ + 2 * pad
+	sizeY = dimY + 2 * pad
+	sizeX = dimX + 2 * pad
+
+	paddedParticle = np.zeros( ( sizeZ,sizeY,sizeX ) )
+	
+	paddedParticle[ pad : dimZ+pad+1, pad : dimY+pad+1, pad : dimX+pad+1] = croppedParticle
+
+	cleanedPaddedParticle = removeOtherLabels(paddedParticle, label)
+
+	# convert label number to ones
+	normalizedCleanedPaddedParticle = cleanedPaddedParticle//label
+
+	return normalizedCleanedPaddedParticle
+
+def analyzeParticles(labMapLoc, sampleName='', skeletonSizeLimit=100, saveData=True, outputDir='',):
+	"""Code for the analyses of label map to obtain the following: 
+		1 - Particle size 
+		2 - Particle network parameters
 	"""
 	ofl=outputDir
 	clm = tf.imread(labMapLoc).astype('uint16')
 	numPtcl = clm.max()
 	print('\nNum particles: ' + str(numPtcl))
-
-	particleData = np.zeros((numPtcl,5)) 	
+	
 	#Index, Surface area, Hull area, volume, hull volume
+	particleData = np.zeros((numPtcl,5)) 	
 
-	for ptclNo in range(3612,3613):
+	ptclList = [2365,1003,1095,2871,501,2177,22,864,3080,2131]
+	
+	for ptclNo in ptclList:
 		
 		print('\nChecking particle ' + str(ptclNo) + '/' + str(numPtcl))	
 
@@ -169,6 +177,7 @@ def analyzeParticles(labMapLoc, sampleName='', saveData=True, outputDir='',):
 
 		# Extract particle subvolume
 		print('\tCropping')
+
 		ptcl = cropAndPadParticle(labelledMap=clm,
 											label=ptclNo,
 											pad= 20,
@@ -178,15 +187,14 @@ def analyzeParticles(labMapLoc, sampleName='', saveData=True, outputDir='',):
 
 		tf.imwrite( (ofl+currFileName+'.tiff'), ptcl.astype('uint8'))
 
-
 		# Generate STL
 		if genSTL == True:
 			print('\tMaking Stl')
-			_generateInPlaceStlFile( ptcl, 
-										stepSize = 1, 
-										saveImg=True, 
-										sampleName=currFileName, 
-										outputDir=ofl)
+			generateInPlaceStlFile( ptcl, 
+									stepSize = 1, 
+									saveImg=True, 
+									sampleName=currFileName, 
+									outputDir=ofl)
 
 		# Getting particle volume
 		if genPtclVol == True:
@@ -194,20 +202,7 @@ def analyzeParticles(labMapLoc, sampleName='', saveData=True, outputDir='',):
 			ptclVol=np.sum(ptcl)
 			particleData[ptclNo-1,3] = ptclVol
 			print('\t\tVolume:', str(ptclVol))
-
-
-		if genSkeleton == True and ptclVol >= 200:
-			print('\tSkeletonizing')
-			ptclSkeleton = skeletonize(ptcl)
-			tf.imwrite( (ofl+currFileName+'-skeleton.tiff'), ptclSkeleton.astype('uint8'))
-
-			# network analyses of skeleton
-			dataForBranch = summarize(Skeleton(ptclSkeleton))
-			dataForBranch.to_csv(ofl+currFileName+'-GraphData.csv',sep=',')
-
-		else: print('\tParticle too small for skeleton...')
-
-
+		
 		# Compute EDM of particle subvolume
 		if genEDM == True:
 			print('\tEDMing')
@@ -217,14 +212,28 @@ def analyzeParticles(labMapLoc, sampleName='', saveData=True, outputDir='',):
 												sampleName=currFileName, 
 												outputDir=ofl )
 
-		# Get product of skeleton EDM
-		if genEDMSkeleton == True:
-			print('\tGetting EDM on skeleton')
-			skeletonEDM = ptclSkeleton*edmPtcl
+		# Generate skeleton
+		if genSkeleton == True and ptclVol >= skeletonSizeLimit:
+			print('\tSkeletonizing')
+			ptclSkeleton = skeletonize(ptcl)
+			tf.imwrite( (ofl+currFileName+'-skeleton.tiff'), ptclSkeleton.astype('uint8'))
 
-			# Get list of ED along skeleton
-			nonZeroEDMVal = (skeletonEDM[np.nonzero(skeletonEDM)]).flatten()
-			np.savetxt(ofl+currFileName+'-edmSkeleton.csv', nonZeroEDMVal, delimiter=',')
+			# weighted skeleton
+			ptclWeightedSkeleton = edmPtcl*ptclSkeleton
+
+			# network analyses of skeleton
+			dataForBranch = summarize(Skeleton(ptclWeightedSkeleton))
+			dataForBranch.to_csv(ofl+currFileName+'-GraphData.csv',sep=',')
+
+			"""The branch type is coded by number as:
+
+			0 - endpoint-to-endpoint (isolated branch)
+			1 - junction-to-endpoint
+			2 - junction-to-junction
+			3 - isolated cycle
+			"""
+
+		else: print('\tParticle too small for skeleton...')
 
 		# Surface area of particles
 		if genSaPtcl == True:
@@ -249,4 +258,4 @@ def analyzeParticles(labMapLoc, sampleName='', saveData=True, outputDir='',):
 
 		np.savetxt(ofl + sampleName+'-Data.csv',particleData,delimiter=',')
 
-analyzeParticles(labMapLoc=clm, sampleName=sampleName, saveData=True, outputDir=ofl,)
+analyzeParticles(labMapLoc=clm, sampleName=sampleName, skeletonSizeLimit = 100, saveData=True, outputDir=ofl)
